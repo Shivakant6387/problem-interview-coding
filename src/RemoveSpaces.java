aaa24a3cbf690c1e16f097a55bea775d930d1b85
Shivakant6387 <shivakant5875@gmail.com>
2023-07-25 17:07:20 +0530

    Remove spaces

---
+++ src/RemoveSpaces.java
@@ -0,0 +1,7 @@
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String str="Hey Teesa";
+       str= str.replaceAll("\\s","");
+        System.out.println(str);
+    }
+}
